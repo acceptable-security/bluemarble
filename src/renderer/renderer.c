ea18a47ccbbc9ecd458a9a3c765084f63c64845f
Brian Smith <block8437@gmail.com>
2016-08-22 14:12:56 -0400

    Fixed some nonissues

--- src/renderer/renderer.c
+++ src/renderer/renderer.c
@@ -304,7 +304,7 @@ void renderer_generate_vertices(renderer_t* renderer) {
         glBindBuffer(GL_ARRAY_BUFFER, 0);
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, renderer->ibo);
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * (width - 1) * (height - 1) * 4, renderer->indices, GL_STATIC_DRAW);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(unsigned int) * (width - 1) * (height - 1) * 4, renderer->indices, GL_STATIC_DRAW);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
